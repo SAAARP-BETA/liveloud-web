bc26df88c521a352db21c387cdc18e85747d1b91
imaayushk <kumaraayush327@gmail.com>
2025-07-04 17:40:44 +0530

    Added auth in create post

--- app/(app)/layout.js
+++ app/(app)/layout.js
@@ -4,7 +4,7 @@ export default function AppLayout({ children }) {
   return (
     <div className="flex">
       <LeftSidebar />
-      <main className="flex-1 lg:ml-70 p-4">{children}</main>
+      <main className="flex-1 p-4">{children}</main>
     </div>
   );
 }
