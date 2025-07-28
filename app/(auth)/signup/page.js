c2744ce9dbdb7a6fb8dd35610b4d7a9c624128e6
Lavanya Dhiman <iamlavanya2004@gmail.com>
2025-07-28 20:36:58 +0530

    eye

--- app/(auth)/signup/page.js
+++ app/(auth)/signup/page.js
@@ -1,17 +1,5 @@
 "use client";
 
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuth } from '@/app/context/AuthContext';
-import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash, faCheck } from '@fortawesome/free-solid-svg-icons';
-import { toast } from 'react-hot-toast'; // <-- ADD THIS
-import google from '@/app/assets/googleicon1.png';
-import metaMask from '@/app/assets/metamaskicon1.png';
-import Logo from '@/app/assets/LiveLoud.svg';
-=======
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/app/context/AuthContext";
@@ -21,7 +9,6 @@ import { faEye, faEyeSlash, faCheck } from "@fortawesome/free-solid-svg-icons";
 import google from "@/app/assets/googleicon1.png";
 import metaMask from "@/app/assets/metamaskicon1.png";
 import Logo from "@/app/assets/LongLogo.png";
->>>>>>> 9a7cf093defe79bf1cc47902fc071271633dcfbf
 
 export default function Signup() {
   const router = useRouter();
@@ -126,7 +113,7 @@ export default function Signup() {
           disabled={isLoading}
         />
         <button
-          className="px-2"
+          className="px-2 cursor-pointer"
           onClick={() => setShowPassword(!showPassword)}
           disabled={isLoading}
         >
